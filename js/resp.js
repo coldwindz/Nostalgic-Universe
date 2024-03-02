d635d429eeffdbdc58d2078fad4cb3feddfcb039
coldwindz <happyindia2005@gmail.com>
2024-03-03 02:58:04 +0530

    go

--- js/resp.js
+++ js/resp.js
@@ -2,10 +2,12 @@ burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
 navList = document.querySelector('.nav-list')
 navRight = document.querySelector('.navRight')
+navlogo = document.querySelector('.logo-resp')
 
 burger.addEventListener('click', ()=>{
     navRight.classList.toggle('v-class-resp');
     navList.classList.toggle('v-class-resp');
     navbar.classList.toggle('h-nav-resp');
+    navlogo.classList.toggle('v-class-resp');
 
 })
