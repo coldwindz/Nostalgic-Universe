67ccd431eb8d8c487a701f7d1ce5ed599be07a10
coldwindz <happyindia2005@gmail.com>
2024-03-03 18:24:35 +0530

    go

--- js/resp.js
+++ js/resp.js
@@ -11,3 +11,11 @@ burger.addEventListener('click', ()=>{
     navlogo.classList.toggle('v-class-resp');
 
 })
+
+// navToggle = document.querySelector('.nav-toggle')
+// bars = document.querySelector('.bar')
+
+// function toggleHamburger(e) {
+//     bars.forEach(bar => bar.classList.toggle('x'))
+// }
+// navToggle.addEventListener('click', toggleHamburger)
